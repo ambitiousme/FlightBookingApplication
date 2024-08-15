fc460d697269390a6386fd609736f09e5f7f1467
Shivam <shibamsharma7777@gmail.com>
2024-08-15 23:38:20 +0530

    Commit:15082024:Initialize Project.

---
+++ FlightBookingApplication/src/test/java/com/lts/FBA/FlightBookingApplication/FlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lts.FBA.FlightBookingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FlightBookingApplication
 For learning purpose
+
+Date:15/08/2024
+Create project by springInitializr. Committing it to GIT.
